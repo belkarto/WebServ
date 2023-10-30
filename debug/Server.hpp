0dee50182ead4dcf2b27f9334f791dde0e0eabd4
brahim elkartouch <belkarto@student.1337.ma>
2023-10-30 02:44:36 +0100

    testing select

--- debug/Server.hpp
+++ debug/Server.hpp
@@ -1,44 +1,42 @@
 #ifndef SERVER_HPP
-# define SERVER_HPP
+#define SERVER_HPP
 
+#include <arpa/inet.h>
+#include <fcntl.h>
 #include <iostream>
-#include <sys/socket.h>
-#include <sys/types.h>
 #include <netdb.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
-#include <unistd.h>
 #include <signal.h>
-#include <arpa/inet.h>
 #include <string.h>
-#include <fcntl.h>
-#include <vector>
 #include <sys/select.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <vector>
+#include <sstream>
 
-# define CHUNK 1024
+#define CHUNK 1024
 
-class Server
-{
-	public:
-		const char			*node;
-		const char			*service;
-		struct addrinfo		*bind_addr;
-		int					sockfd;
+class Server {
+public:
+  const char *node;
+  const char *service;
+  struct addrinfo *bind_addr;
+  int sockfd;
 
-		Server(const char* node, const char* service);
-		void	setupServer(void);
+  Server(const char *node, const char *service);
+  void setupServer(void);
 };
 
-typedef struct
-{
+typedef struct {
   int socket_fd;
   std::vector<Server> servers_vec;
-  fd_set currentFdSet;
-  fd_set tmpFdSet;
+  fd_set masterSet;
+  fd_set workinSet;
   int max_socket;
   int ready_size;
 } t_pool;
 
-void		send_response(int sockfd);
-std::string	get_headers(int sockfd);
+std::string get_headers(int sockfd);
 #endif
--- debug/main.cpp
+++ debug/main.cpp
@@ -2,42 +2,85 @@
 #include <cstdio>
 #include <cstdlib>
 
+void init_servers_pool(t_pool &serversPool);
+void errAnExit(const char *er);
+void readRequest(int socketFd);
+void sendReponse(int socketFD);
+void getRequestAndRespond(int socketFd, t_pool serversPool,
+                          int connectedSocket);
+
+int main() {
+  t_pool serversPool;
+  int connFd;
+  init_servers_pool(serversPool);
+  while (1) {
+    serversPool.workinSet = serversPool.masterSet;
+    std::cout << "waiting on select....." << std::endl;
+
+    if ((serversPool.ready_size =
+             select(serversPool.max_socket, &serversPool.workinSet, NULL, NULL,
+                    NULL)) < 0)
+      errAnExit("select()");
+
+    std::cout << "ready sockets -------> " << serversPool.ready_size
+              << std::endl;
+    for (int i = 0; i < serversPool.max_socket && serversPool.ready_size > 0;
+         i++) {
+      if (FD_ISSET(i, &serversPool.workinSet)) {
+        serversPool.ready_size -= 1;
+        if ((connFd = accept(i, NULL, NULL)) < 0)
+          errAnExit("accept()");
+        std::cout << connFd << " " << i << std::endl;
+        FD_SET(connFd, &serversPool.masterSet);
+        if (connFd > serversPool.max_socket)
+          serversPool.max_socket = connFd;
+        getRequestAndRespond(i, serversPool, connFd);
+        std::cout << "[[[[[[[--  client served  --]]]]]]]\n\n\n" << std::endl;
+      }
+    }
+  }
+  return (EXIT_SUCCESS);
+}
+
+void  sendReponse(int socketFd)
+{
+
+}
+
+void readRequest(int socketFd) {
+  char buffer[4096];
+  size_t readed;
+
+  std::cout << "getting request... from " << socketFd << std::endl;
+  if ((readed = read(socketFd, buffer, 4096)) < 0)
+    errAnExit("read()");
+  std::cout << readed << " readed from request" << std::endl;
+  write(1, buffer, readed);
+}
+
+void getRequestAndRespond(int socketFd, t_pool serversPool,
+                          int connectedSocket) {
+  std::cout << connectedSocket << std::endl;
+  readRequest(connectedSocket);
+  sendReponse(connectedSocket);
+  close(connectedSocket);
+  FD_CLR(connectedSocket, &serversPool.masterSet);
+}
+
 void errAnExit(const char *er) {
   perror(er);
   exit(EXIT_FAILURE);
 }
 
 void init_servers_pool(t_pool &serversPool) {
-  serversPool.servers_vec.emplace_back("localhost", "1026");
-  serversPool.servers_vec.emplace_back("127.0.0.1", "1025");
-  serversPool.servers_vec.emplace_back("127.0.0.2", "1025");
-  serversPool.servers_vec.emplace_back("127.0.0.3", "1025");
+  size_t i;
+  serversPool.servers_vec.emplace_back("127.0.0.1", "3433");
+  serversPool.servers_vec.emplace_back("127.0.0.1", "3434");
+  serversPool.servers_vec.emplace_back("127.0.0.1", "3435");
+  serversPool.servers_vec.emplace_back("127.0.0.1", "3436");
 
-  FD_ZERO(&serversPool.currentFdSet);
-  FD_SET(serversPool.servers_vec[0].sockfd, &serversPool.currentFdSet);
-  FD_SET(serversPool.servers_vec[1].sockfd, &serversPool.currentFdSet);
-  FD_SET(serversPool.servers_vec[2].sockfd, &serversPool.currentFdSet);
-  FD_SET(serversPool.servers_vec[3].sockfd, &serversPool.currentFdSet);
-  serversPool.max_socket = serversPool.servers_vec[3].sockfd;
-}
-
-int main() {
-  t_pool serversPool;
-
-  init_servers_pool(serversPool);
-  while (1) {
-    serversPool.tmpFdSet = serversPool.currentFdSet;
-    if ((serversPool.ready_size =
-             select(serversPool.max_socket, &serversPool.tmpFdSet, NULL, NULL,
-                    NULL)) < 0)
-      errAnExit("select()");
-    std::cout << "ready sockets " << serversPool.ready_size << std::endl;
-    for (int i = 0 ; i < serversPool.max_socket; i++) {
-      if (FD_ISSET(i, &serversPool.tmpFdSet))
-      {
-
-      }
-    }
-  }
-  return (EXIT_SUCCESS);
+  FD_ZERO(&serversPool.masterSet);
+  for (i = 0; i < serversPool.servers_vec.size(); i++)
+    FD_SET(serversPool.servers_vec[i].sockfd, &serversPool.masterSet);
+  serversPool.max_socket = serversPool.servers_vec[i - 1].sockfd + 1;
 }
