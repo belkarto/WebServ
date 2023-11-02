531a720d05c64d697804819cf3cb9461fc0225eb
Ilham Salhi <isalhi@c4r6p10.1337.ma>
2023-11-02 09:46:19 +0100

    working with epoll for multiplexing and buffering => works fine with 240k files

--- include/Server.hpp
+++ include/Server.hpp
@@ -26,7 +26,7 @@ class Server
 		std::vector<std::string>					server_name;
 		std::vector<std::string>					index;
 		std::string									root;
-		long long									client_max_body_size;
+		long long										client_max_body_size;
 		std::map<std::vector<int>, std::string >	error_page;
 		bool										autoindex;
 		std::vector<Location>						location;
--- src/Client.cpp
+++ src/Client.cpp
@@ -1,2 +1,27 @@
 #include "Client.hpp"
 
+
+Client::Client(int sockfd, int listen_socket)
+{
+	this->sockfd = sockfd;
+	this->listen_socket = listen_socket;
+	this->recieved = 0;
+	this->sent = 0;
+	this->response = NULL;
+	this->response_size = 0;
+	this->headers = "";
+	this->sending = false;
+	this->path = "";
+}
+
+void    Client::resetState()
+{
+	delete this->response;
+	this->recieved = 0;
+	this->sent = 0;
+	this->response = NULL;
+	this->response_size = 0;
+	this->headers.clear();
+	this->sending = false;
+	this->path = "";
+}
