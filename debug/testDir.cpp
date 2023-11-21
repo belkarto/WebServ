8f12ae87ed9b74dc565a88c61a493897b56e9220
brahim elkartouch <belkarto@student.1337.ma>
2023-11-21 16:53:12 +0100

    test in debug
    Former-commit-id: 686856759120220b5d30b4c69e814354859ff301

---
+++ debug/testDir.cpp
@@ -0,0 +1,32 @@
+
+#include <dirent.h>
+#include <iostream>
+
+int main() {
+  const char *directoryName = "/nfs/homes/belkarto/Desktop/"; // Current directory
+
+  DIR *dir = opendir(directoryName);
+
+  if (dir) {
+    struct dirent *entry;
+
+    while ((entry = readdir(dir)) != NULL) {
+      std::cout << entry->d_name;
+      if (entry->d_type == DT_REG)
+        std::cout << " regular";
+      else if (entry->d_type == DT_DIR)
+        std::cout << " Dir";
+      else if (entry->d_type == DT_LNK)
+        std::cout << " symbol_link";
+      std::cout << std::endl;
+      // << " " << entry->d_ino << " " << entry->d_off << std::endl;
+    }
+
+    closedir(dir);
+  } else {
+    std::cerr << "Error opening directory." << std::endl;
+    return 1;
+  }
+
+  return 0;
+}
