b0787677f1b1a577fc0cff8ea8a1eddf76e171b3
Ilham Salhi <isalhi@c4r6p10.1337.ma>
2023-11-21 16:34:53 +0100

    working on response
    Former-commit-id: e72821f7c93f923af2c6252ffffa01bf1b471d2f

--- src/Response.cpp
+++ src/Response.cpp
@@ -1,8 +1,17 @@
-#include "Response.hpp"
+#include "Client.hpp"
 
 void    Response::setErrorResponse(CLIENTIT& clientIt)
 {
-	(void) clientIt;
+	std::map<std::vector<int>, std::string>::iterator	it,ite;
+	std::stringstream									ss;
+	int													code;
+
+	ss << status;
+	ss >> code;
+	it = clientIt->serverIt->error_page.begin();
+	ite = clientIt->serverIt->error_page.end();
+	for (; it != ite && find(it->first.begin(), it->first.end(), code) == it->first.end(); it++ );
+	filePath = (it != ite) ? it->second : it->second;
 }
 
 void    Response::setGetResponse(CLIENTIT& clientIt)
